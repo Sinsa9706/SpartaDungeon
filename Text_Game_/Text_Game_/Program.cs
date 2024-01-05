87ea40bfdf9bf6bd1ef948fae22e0eac7b706ed7
Sinsa9706 <154485256+Sinsa9706@users.noreply.github.com>
2024-01-05 17:54:26 +0900

    Add files via upload

---
+++ Text_Game_/Text_Game_/Program.cs
@@ -0,0 +1,231 @@
+﻿using System.Net.Security;
+using System.Reflection.Emit;
+using System.Reflection.PortableExecutable;
+using System.Xml.Linq;
+using static System.Console;
+using static Text_Game_.Program;
+
+
+//    public void CharacterInfo()
+//    {
+//        WriteLine($"Lv. {level:D2}");
+//        WriteLine($"{name} ( {job} )");
+//        WriteLine($"공격력 : {atk}");
+//        WriteLine($"방어력 : {def}");
+//        WriteLine($"체 력 : {hp}");
+//        WriteLine($"Gold : {gold} G");
+//    }
+//}
+
+//class Player : Character
+//{
+//    static void DisplayPlayerInfo()
+//    {
+//        Clear();
+//        ("상태 보기").PrintWithColor(ConsoleColor.Yellow, true);
+//        WriteLine("캐릭터의 정보가 표시됩니다.");
+//        WriteLine();
+
+//        player.DisplayPlayerInfo();
+//    }
+
+namespace Text_Game_
+{
+    // 확장 메서드는 최상위 정적 클래스에 위치해야함
+    public static class ExtensionString
+    {
+        // string의 확장 메서드
+        // 텍스트 색상을 변경하여 출력해줌
+        // 콘솔에 입력할 텍스트, 색상, WriteLine or Write (bool 값으로 정의)
+        public static void PrintWithColor(this string text, ConsoleColor color, bool isEnter)
+        {
+            ForegroundColor = color;
+            if (isEnter) WriteLine(text); else Write(text);
+            ResetColor();
+        }
+    }
+
+    internal class Program
+    {
+        static Character player;
+        static Item[] items;
+
+        static int startMe;
+
+        static void Main(string[] args)
+        {
+            GameSetting();
+
+            while (true)
+            {
+                if (startMe == 0) DisplayStartMenu();
+                else if (startMe == 1) DisplayCharacterInfo();
+                else if (startMe == 2) DisplayInventory();
+            }
+        }
+
+        // 게임 데이터 설정
+        static void GameSetting()
+        {
+            player = new Character(10, "Sinsa", "전사", 10, 5, 100, 1500);
+
+            items = new Item[10];
+
+        }
+
+        // 메인 화면 출력 메서드
+        static void DisplayStartMenu()
+        {
+            Clear();
+            WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
+            WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
+            WriteLine();
+            ("1. ").PrintWithColor(ConsoleColor.Magenta, false); WriteLine("상태 보기");
+            ("2. ").PrintWithColor(ConsoleColor.Magenta, false); WriteLine("인벤토리");
+            ("3. ").PrintWithColor(ConsoleColor.Magenta, false); WriteLine("상점");
+            WriteLine();
+            startMe = GetPlayerSelect(1, 3);
+        }
+
+        // 캐릭터 상태 표시
+        static void DisplayCharacterInfo()
+        {
+            Clear();
+            ("상태 보기").PrintWithColor(ConsoleColor.Yellow, true);
+            WriteLine("캐릭터의 정보가 표시됩니다.");
+            WriteLine();
+            Write("Lv. "); ($"{player.Level}").PrintWithColor(ConsoleColor.Magenta, true);
+            WriteLine($"{player.Name} ( {player.Job} ) ");
+            Write("공격력 : "); ($"{player.Atk}").PrintWithColor(ConsoleColor.Magenta, true);
+            Write("방어력 : "); ($"{player.Def}").PrintWithColor(ConsoleColor.Magenta, true);
+            Write("체 력 : "); ($"{player.Hp}").PrintWithColor(ConsoleColor.Magenta, true);
+            Write("Gold : "); ($"{player.Gold}").PrintWithColor(ConsoleColor.Magenta, true);
+            ("0. ").PrintWithColor(ConsoleColor.Magenta, false); WriteLine("나가기");
+            WriteLine();
+            startMe = GetPlayerSelect(0, 0);
+        }
+
+        // 인벤토리 표시
+        static void DisplayInventory()
+        {
+            Clear();
+            ("인벤토리 - 장착 관리").PrintWithColor(ConsoleColor.Yellow, true);
+            WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+            WriteLine();
+            WriteLine("[아이템 목록]");
+        }
+
+        // 메뉴 선택
+        static int GetPlayerSelect(int start, int end)
+        {
+            WriteLine("원하시는 행동을 입력해주세요.");
+            (">> ").PrintWithColor(ConsoleColor.Yellow, false);
+            int select = 0;
+            bool isNum = false;
+            while (true)
+            {
+                isNum = int.TryParse(ReadLine(), out select);
+                if (!isNum || (select < start || select > end))
+                {
+                    ("잘못된 입력입니다. 다시 고르세요").PrintWithColor(ConsoleColor.Red, true);
+                }
+                else break;
+            }
+            return select;
+        }
+
+        public class Character
+        {
+            public int Level { get; }
+            public string Name { get; }
+            public string Job { get; }
+            public int Atk { get; }
+            public int Def { get; }
+            public int Hp { get; }
+            public int Gold { get; }
+
+            public Character(int level, string name, string job, int atk, int def, int hp, int gold)
+            {
+                Level = level;
+                Name = name;
+                Job = job;
+                Atk = atk;
+                Def = def;
+                Hp = hp;
+                Gold = gold;
+            }
+        }
+
+        public class Item
+        {
+            public string Name { get; }
+            public string Description { get; }
+            public int Type { get; }
+            public int Atk { get; }
+            public int Def { get; }
+            public int Gold { get; }
+            public bool IsEquiped { get; set; }
+            public static int ItemCount = 0;
+
+            public Item(string name, string description, int type, int atk, int def, int gold, bool isEquiped)
+            {
+                Name = name;
+                Description = description;
+                Type = type;
+                Atk = atk;
+                Def = def;
+                Gold = gold;
+                IsEquiped = isEquiped;
+            }
+
+            public void ItemDescription(bool withNumber = false, int i = 0)
+            {
+                Write("- ");
+                if (withNumber)
+                {
+                    ($"{i}").PrintWithColor(ConsoleColor.Magenta, false);
+                }
+                if (IsEquiped)
+                {
+                    Write("["); ("E").PrintWithColor(ConsoleColor.Cyan, false); Write("]");
+                    Write(PadRightForMixedText(Name, 9));
+                }
+                else Write(PadRightForMixedText(Name, 12));
+
+                Write(" | ");
+
+                if (Atk != 0) Write($"Atk {(Atk >= 0 ? "+" : "")}{Atk} ");
+                if (Def != 0) Write($"Def {(Def >= 0 ? "+" : "")}{Def} ");
+
+                Write(" | ");
+
+                WriteLine(Description);
+            }
+            
+            // 아이템 이름의 줄맞춤을 위한 메서드
+            public static int GetPrintableLength(string str)
+            {
+                int length = 0;
+                foreach  (char c in str)
+                {
+                    if(char.GetUnicodeCategory(c) == System.Globalization.UnicodeCategory.OtherLetter)
+                    {
+                        length += 2;
+                    }
+                    else
+                    {
+                        length += 1;
+                    }
+                }
+                return length;
+            }
+
+            public static string PadRightForMixedText(string str, int totalLength)
+            {
+                int currentLength = GetPrintableLength(str);
+                int padding = totalLength - currentLength;
+                return str.PadRight(str.Length + padding);
+            }
+        }
+    }
+}
